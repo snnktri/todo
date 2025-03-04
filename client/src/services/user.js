ee327ee5c1b39a67006912932943bf05506cf494
snnktri <skktri237@gmail.com>
2025-03-04 16:55:51 +0545

    user service add

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -9,6 +9,16 @@ export const signUp = async(data) => {
         console.log(response.data);
         return response.data;
     } catch (error) {
+        console.error("Error on sign up: ", error);
+    }
+}
+
+export const login = async(logindata) => {
+    try {
+        const response = await api.post("/users/login", logindata)
+        console.log(response.data);
+        localStorage.setItem("user", response.data.data.accessToken);
+    } catch (error) {
         console.error("Error on login: ", error);
     }
 }
